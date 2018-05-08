9264edade547964130944112d47629d50eb3214a
Dylan On <dylanon@users.noreply.github.com>
2018-05-07 21:56:33 -0400

    Add RSVP section

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import '../sass/style.scss';
+
+const copyRsvpEmail = () => {
+  const email = document.querySelector('.rsvp__email');
+  window.getSelection().selectAllChildren(email);
+  document.execCommand('copy');
+  const copyMessage = document.querySelector('.rsvp__copy-message');
+  copyMessage.textContent = 'Copied!';
+};
+
+const events = () => {
+  // Listen for click to copy email address
+  const rsvpEmail = document.querySelector('.rsvp__email');
+  rsvpEmail.addEventListener('click', copyRsvpEmail);
+  const copyEmailTrigger = document.querySelector('.rsvp__copy');
+  copyEmailTrigger.addEventListener('click', copyRsvpEmail);
+};
+
+window.addEventListener('load', () => {
+  events();
+});
