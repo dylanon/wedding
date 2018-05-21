5434a89fbd60ab41f298d7f216431e004ce72d2d
Dylan On <dylanon@users.noreply.github.com>
2018-05-21 18:56:51 -0400

    Add menu

--- src/js/index.js
+++ src/js/index.js
@@ -16,23 +16,19 @@ const getYPositionOfMainContent = () => {
 };
 
 const events = () => {
-  // // Initialize smooth scrolling for anchors
-  // const scroll = new smoothScroll('a[href*="#"]');
   // Listen for click to scroll to main content
   const headerLogo = document.querySelector('.header__logo');
   const skipIcon = document.querySelector('.header__skip-to-main');
   const skipElements = [headerLogo, skipIcon];
   const htmlElement = document.querySelector('html');
   const yPosition = getYPositionOfMainContent();
-  let scrollToIntro;
+  let scrollToMain;
   if ('scrollBehavior' in htmlElement.style) {
-    scrollToIntro = () => {
-      console.log('Performed native smooth scroll.');
+    scrollToMain = () => {
       window.scrollTo(0, yPosition);
     };
   } else {
-    scrollToIntro = () => {
-      console.log('Falling back to scroll-smooth library.');
+    scrollToMain = () => {
       const scroll = new smoothScroll();
       scroll.animateScroll(yPosition, undefined, {
         speed: 800,
@@ -41,15 +37,51 @@ const events = () => {
     };
   }
   skipElements.forEach(element => {
-    element.addEventListener('click', scrollToIntro);
+    element.addEventListener('click', scrollToMain);
   });
   // Listen for click to copy email address
   const rsvpEmail = document.querySelector('.rsvp__email');
   rsvpEmail.addEventListener('click', copyRsvpEmail);
   const copyEmailTrigger = document.querySelector('.rsvp__copy');
   copyEmailTrigger.addEventListener('click', copyRsvpEmail);
+  // Listen for a click on the menu button
+  const menuButton = document.querySelector('.header__menu-button');
+  menuButton.addEventListener('click', () => {
+    const menu = document.querySelector('.menu');
+    menu.style.transform = 'translateY(0)';
+    menu.style.opacity = '1';
+  });
+  // Listen for click on the menu close button
+  const menuClose = document.querySelector('.menu__close-control');
+  menuClose.addEventListener('click', () => {
+    const menu = document.querySelector('.menu');
+    menu.style.opacity = '0';
+    menu.style.transform = 'translateY(-100%)';
+  });
+  // Listen for click on menu links
+  const menuLinks = document.querySelectorAll('.menu__link');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      const menu = document.querySelector('.menu');
+      menu.style.opacity = '0';
+      menu.style.transform = 'translateY(-100%)';
+    });
+  });
 };
 
+const initSmoothScrolling = () => {
+  const htmlElement = document.querySelector('html');
+  if (!('scrollBehavior' in htmlElement.style)) {
+    // Initialize smooth scrolling for all anchors
+    const scroll = new smoothScroll('a[href*="#"]');
+    console.info(
+      'CSS property `scroll-behavior` is not supported by this browser. Falling back to smooth-scroll.'
+    );
+  }
+};
+
+// Initialize JavaScript when all resources have been loaded
 window.addEventListener('load', () => {
   events();
+  initSmoothScrolling();
 });
